afe9bcf2b91c9583c6bc01fc8b7b73dd1fc7050b
suhyun3301 <iam3301@naver.com>
2022-08-16 23:24:42 +0900

    오류수정: 오브젝트 -> 배열, ...posts

--- src/Community.js
+++ src/Community.js
@@ -1,40 +1,59 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 function Community() {
   const input = useRef(null)
   const textarea = useRef(null)
+  const [Posts, setPosts] = useState([])
 
   const createPost = () => {
-    console.log(input.current.value)
-    console.log(textarea.current.value)
+    setPosts([
+      ...Posts,
+      {
+        title: input.current.value,
+        content: textarea.current.value,
+      },
+    ])
   }
 
   return (
     <section className="community">
-      <div className="input-box">
-        <label htmlFor="title">title</label>
-        <input type="text" placeholder="Enter Title" ref={input} />
+      <div className="create">
+        <div className="input-box">
+          <label htmlFor="title">title</label>
+          <input type="text" placeholder="Enter Title" ref={input} />
+        </div>
+
+        <div className="input-box">
+          <label htmlFor="task">task</label>
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="3"
+            placeholder="Enter content"
+            ref={textarea}
+          ></textarea>
+        </div>
+
+        <div className="btn-box">
+          <button className="btn" type="button">
+            Cancel
+          </button>
+
+          <button className="btn" type="button" onClick={createPost}>
+            Registration
+          </button>
+        </div>
       </div>
 
-      <div className="input-box">
-        <label htmlFor="task">task</label>
-        <textarea
-          name=""
-          id=""
-          cols="30"
-          rows="3"
-          placeholder="Enter content"
-          ref={textarea}
-        ></textarea>
-      </div>
-
-      <div className="btn-box">
-        <button className="btn" type="button">
-          Cancel
-        </button>
-
-        <button className="btn" type="button" onClick={createPost}>
-          Registration
-        </button>
+      <div className="show">
+        {Posts.map((post, i) => {
+          return (
+            <div className="post-list" key={i}>
+              <h2>{post.title}</h2>
+              <p>{post.content}</p>
+            </div>
+          )
+        })}
       </div>
     </section>
   )

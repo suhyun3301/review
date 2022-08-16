27a1a1ebdca4247a0263d3427a3d50c9cc7fd7f3
suhyun3301 <iam3301@naver.com>
2022-08-16 22:58:10 +0900

    input 값 콘솔에 출력

--- src/Community.js
+++ src/Community.js
@@ -1,9 +1,18 @@
+import { useRef } from 'react'
 function Community() {
+  const input = useRef(null)
+  const textarea = useRef(null)
+
+  const createPost = () => {
+    console.log(input.current.value)
+    console.log(textarea.current.value)
+  }
+
   return (
     <section className="community">
       <div className="input-box">
         <label htmlFor="title">title</label>
-        <input type="text" placeholder="Enter Title" />
+        <input type="text" placeholder="Enter Title" ref={input} />
       </div>
 
       <div className="input-box">
@@ -14,14 +23,16 @@ function Community() {
           cols="30"
           rows="3"
           placeholder="Enter content"
+          ref={textarea}
         ></textarea>
       </div>
+
       <div className="btn-box">
         <button className="btn" type="button">
           Cancel
         </button>
 
-        <button className="btn" type="button">
+        <button className="btn" type="button" onClick={createPost}>
           Registration
         </button>
       </div>

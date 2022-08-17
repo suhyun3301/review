7092d56d5296109d5a213b86fb9e74b3bbb13e50
suhyun3301 <iam3301@naver.com>
2022-08-18 00:05:55 +0900

    swiper 플러그인 적용

--- src/main/Vids.js
+++ src/main/Vids.js
@@ -1,5 +1,35 @@
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Mousewheel, Pagination } from 'swiper'
+import 'swiper/css'
+import 'swiper/css/pagination'
+
 function Vids() {
-  return <section className="vids content-box">Video</section>
+  return (
+    <section className="vids content-box">
+      <h2>Video</h2>
+      <Swiper
+        direction={'horizontal'}
+        slidesPerView={2}
+        spaceBetween={30}
+        mousewheel={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Mousewheel, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div className="inner">Slide 1</div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="inner">Slide 2</div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="inner">Slide 3</div>
+        </SwiperSlide>
+      </Swiper>
+    </section>
+  )
 }
 
 export default Vids

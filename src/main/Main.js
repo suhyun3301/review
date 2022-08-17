18a072805acd5ac3c1fe0127d2d4b140940311f3
suhyun3301 <iam3301@naver.com>
2022-08-17 20:38:32 +0900

    메인컨텐츠별 offsetTop값 받아오기

--- src/main/Main.js
+++ src/main/Main.js
@@ -3,16 +3,27 @@ import News from './News'
 import Pic from './Pic'
 import Vids from './Vids'
 import Btns from './Btns'
+import { useEffect, useRef } from 'react'
 
 function Main() {
+  const main = useRef(null)
+
+  useEffect(() => {
+    const sections = main.current.querySelectorAll('.content-box')
+    for (const section of sections) {
+      console.log(section.offsetTop)
+    }
+    console.log(sections)
+  }, [])
+
   return (
-    <>
+    <main ref={main}>
       <Visual />
       <News />
       <Pic />
       <Vids />
       <Btns />
-    </>
+    </main>
   )
 }
 

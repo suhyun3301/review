334349f999518cb039d7b9faa488b1d0c3b97dbf
suhyun3301 <iam3301@naver.com>
2022-08-17 20:41:07 +0900

    Anime 연결

--- src/main/Main.js
+++ src/main/Main.js
@@ -4,6 +4,7 @@ import Pic from './Pic'
 import Vids from './Vids'
 import Btns from './Btns'
 import { useEffect, useRef } from 'react'
+import '../assets/Anime'
 
 function Main() {
   const main = useRef(null)

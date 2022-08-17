3d7bcbd6c47a9b436681ac33e42f646124ff0ee6
suhyun3301 <iam3301@naver.com>
2022-08-18 00:27:23 +0900

    메인컴포넌트에서 데이터를 전달 받기

--- src/main/News.js
+++ src/main/News.js
@@ -1,5 +1,31 @@
+import { useEffect, useState } from 'react'
+
 function News() {
-  return <section className="news content-box">News</section>
+  const [Posts, setPosts] = useState([])
+
+  useEffect(() => {
+    let data = localStorage.getItem('post')
+    data = JSON.parse(data)
+    setPosts(data)
+  }, [])
+
+  return (
+    <section className="news content-box">
+      <h2>News</h2>
+      <div className="post-list">
+        {Posts.map((post, i) => {
+          if (i >= 3) return
+
+          return (
+            <article key={i}>
+              <h3>{post.title}</h3>
+              <p>{post.content}</p>
+            </article>
+          )
+        })}
+      </div>
+    </section>
+  )
 }
 
 export default News

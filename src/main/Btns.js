9d4b6a822f1bf44e062197b56acbb2c076df78c6
suhyun3301 <iam3301@naver.com>
2022-08-17 23:24:27 +0900

    스크롤 시 버튼 활성화 완료

--- src/main/Btns.js
+++ src/main/Btns.js
@@ -2,30 +2,51 @@ import Anime from '../assets/Anime'
 import { useEffect, useRef, useState } from 'react'
 
 function Btns({ setIndex }) {
-  const btns = useRef(null)
+  const btnsRef = useRef(null)
   const offsetTop = useRef([])
   const num = 4
   const speed = 500
 
   const getOffsetTop = () => {
     offsetTop.current = []
-    const sections = btns.current.parentElement.querySelectorAll('.content-box')
+    const sections =
+      btnsRef.current.parentElement.querySelectorAll('.content-box')
     for (const section of sections) {
       offsetTop.current.push(section.offsetTop)
     }
   }
 
+  const activation = () => {
+    const btns = btnsRef.current.children
+    const sections =
+      btnsRef.current.parentElement.querySelectorAll('.content-box')
+    const scroll = window.scrollY
+    const base = -window.innerHeight / 4
+
+    offsetTop.current.map((pos, i) => {
+      if (scroll >= pos + base) {
+        for (const btn of btns) btn.classList.remove('on')
+        for (const section of sections) section.classList.remove('on')
+        btns[i].classList.add('on')
+        sections[i].classList.add('on')
+      }
+    })
+  }
+
   useEffect(() => {
+    btnsRef.current.children[0].classList.add('on')
     getOffsetTop()
     window.addEventListener('resize', getOffsetTop)
+    window.addEventListener('scroll', activation)
 
     return () => {
       window.removeEventListener('resize', getOffsetTop)
+      window.removeEventListener('scroll', activation)
     }
   }, [])
 
   return (
-    <ul className="scroll-btns" ref={btns}>
+    <ul className="scroll-btns" ref={btnsRef}>
       {Array(num)
         .fill()
         .map((_, i) => {

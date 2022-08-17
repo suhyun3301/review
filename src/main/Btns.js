1c705472ab47e7564b51f7af03ff8b7b22c2505d
suhyun3301 <iam3301@naver.com>
2022-08-17 22:17:51 +0900

    스크롤 모션 수정

--- src/main/Btns.js
+++ src/main/Btns.js
@@ -1,35 +1,49 @@
+import Anime from '../assets/Anime'
+import { useEffect, useRef, useState } from 'react'
+
 function Btns({ setIndex }) {
+  const btns = useRef(null)
+  const offsetTop = useRef([])
+  const num = 4
+  const speed = 500
+
+  const getOffsetTop = () => {
+    offsetTop.current = []
+    const sections = btns.current.parentElement.querySelectorAll('.content-box')
+    for (const section of sections) {
+      offsetTop.current.push(section.offsetTop)
+    }
+  }
+
+  useEffect(() => {
+    getOffsetTop()
+    window.addEventListener('resize', getOffsetTop)
+
+    return () => {
+      window.removeEventListener('resize', getOffsetTop)
+    }
+  }, [])
+
   return (
-    <ul className="scroll-btns">
-      <li
-        className="on"
-        onClick={() => {
-          setIndex(1)
-        }}
-      >
-        1
-      </li>
-      <li
-        onClick={() => {
-          setIndex(2)
-        }}
-      >
-        2
-      </li>
-      <li
-        onClick={() => {
-          setIndex(3)
-        }}
-      >
-        3
-      </li>
-      <li
-        onClick={() => {
-          setIndex(4)
-        }}
-      >
-        4
-      </li>
+    <ul className="scroll-btns" ref={btns}>
+      {Array(num)
+        .fill()
+        .map((_, i) => {
+          return (
+            <li
+              key={i}
+              onClick={() => {
+                new Anime(window, {
+                  prop: 'scroll',
+                  value: offsetTop.current[i],
+                  duration: speed,
+                })
+              }}
+            >
+              {i + 1}
+            </li>
+          )
+        })}
     </ul>
   )
 }
--- src/main/Main.js
+++ src/main/Main.js
@@ -3,38 +3,15 @@ import News from './News'
 import Pic from './Pic'
 import Vids from './Vids'
 import Btns from './Btns'
-import Anime from '../assets/Anime'
-import { useEffect, useRef, useState } from 'react'
 
 function Main() {
-  const main = useRef(null)
-  const offsetTop = useRef([])
-  const [Index, setIndex] = useState(0)
-
-  const getOffsetTop = () => {
-    const sections = main.current.querySelectorAll('.content-box')
-    for (const section of sections) {
-      offsetTop.current.push(section.offsetTop)
-    }
-  }
-
-  useEffect(() => {
-    getOffsetTop()
-
-    new Anime(window, {
-      prop: 'scroll',
-      value: offsetTop.current[Index],
-      duration: 500,
-    })
-  }, [Index])
-
   return (
-    <main ref={main}>
+    <main>
       <Visual />
       <News />
       <Pic />
       <Vids />
-      <Btns setIndex={setIndex} />
+      <Btns />
     </main>
   )
 }
